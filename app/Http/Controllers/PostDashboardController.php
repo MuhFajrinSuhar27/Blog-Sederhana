315937a47db6a2130d61d2926d1fada153c6b358
unknown <fajrinsuhar97@gmail.com>
2025-09-25 08:57:05 +0800

    feat:add to UI post Dashboard

--- app/Http/Controllers/PostDashboardController.php
+++ app/Http/Controllers/PostDashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostDashboardController extends Controller
 {
@@ -11,7 +12,7 @@ class PostDashboardController extends Controller
      */
     public function index()
     {
-        return view('dashboard');
+        return view('dashboard', ['posts' => Post::latest()->paginate(17)]);
     }
 
     /**
